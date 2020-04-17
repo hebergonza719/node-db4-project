eaafd1647795ba44835ff748cbf2cf6147b835d2
Heber <hebergonza@gmail.com>
2020-04-16 22:13:26 -0400

    added model and router, added db-config.js, modified knexfile.js and server.js

---
+++ recipes/recipes-router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const Recipes = require('./recipes-model.js');
+
+const router = express.Router();
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-// const SchemeRouter = require('./schemes/scheme-router.js');
+const RecipesRouter = require('./recipes/recipes-router.js');
 
 const server = express();
 
 server.use(express.json());
-// server.use('/api/schemes', SchemeRouter);
+
+server.use('/api/recipes', RecipesRouter);
 
 module.exports = server;
